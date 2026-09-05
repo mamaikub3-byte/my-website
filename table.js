89d8a9d076a5db115847e6f370e9cfb331f1c68a
mamaikub3-byte <mamaikub3@gmail.com>
2026-09-05 21:09:15 +0700

    Add files via upload

---
+++ table.js
@@ -0,0 +1,11 @@
+
+var table =document.createElement('table')
+document.getElementById("data-list").appendChild(table);
+function changeColor(){
+    const btn = document.getElementById("my button");
+    btn.style.backgroundColor = "red";
+    btn.style.color = "white";
+    const container = document.querySelector('.container');
+    container.classList.add('center-container');
+    
+}
